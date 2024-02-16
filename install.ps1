a519a31d2ce3c685e541f88be34aa3acfabb13fd
Hexality <hexalitymusic@gmail.com>
2024-02-16 02:15:52 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,8 @@
 #requires -version 7
 $POSHPA = Split-Path -Parent $PROFILE
-[void]((Test-Path $POSHPA) ?? (mkdir -p $POSHPA))
+if(!(Test-Path $POSHPA)){ 
+  mkdir -p $POSHPA
+}
 $FETCHFI = Join-Path $POSHPA fetch.ps1
 Invoke-RestMethod -Uri https://raw.githubusercontent.com/Hexality/fetch/main/fetch.ps1 > $FETCHFI
 "`n. $FETCHFI" >> $PROFILE
