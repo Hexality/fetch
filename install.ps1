0d9e8e1a4ee6d8a5db5063202f849bdf58df86f6
Hexality <hexalitymusic@gmail.com>
2024-02-16 03:48:59 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -6,3 +6,5 @@ if(!(Test-Path $POSHPA)){
 $FETCHFI = Join-Path $POSHPA fetch.ps1
 Invoke-RestMethod -Uri https://raw.githubusercontent.com/Hexality/fetch/main/fetch.ps1 > $FETCHFI
 "`n. $FETCHFI" >> $PROFILE
+. $PROFILE
+if(Test-Path $FETCHFI) { Write-Host "Successfully installed, use 'fetch' to invoke it. (use -noclear after the command to avoid it from clearing the console)" } else { "Something gone wrong..." }
